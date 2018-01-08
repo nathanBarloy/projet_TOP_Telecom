656dbc3f3c02b630a18b083a3eefb354da248e40
Nathan Barloy <nathan.barloy@telecomnancy.eu>
2018-01-08 16:33:28 +0100

    ajout commentaires

--- TOP-gr15/src/pxparpx/part1.scala
+++ TOP-gr15/src/pxparpx/part1.scala
@@ -7,19 +7,24 @@ object part1 {
   }
   
   def greyLevel(src:Array[Array[Int]] , coeffR:Double=0.299 , coeffG:Double=0.587 , coeffB:Double=0.114):Unit={
+    /* ce programme converti une image couleur en niveau de gris.
+     * les coefficients de chaque couleur peuvent etre modifiés.
+     */
     var height=src.length
     var width=src(0).length
-    for (i<-0 to height-1) {
+    for (i<-0 to height-1) { 
       for (j<-0 to width-1) {
         var (t,r,g,b)=convert(src(i)(j))
-        var misNiveau=(coeffR*r+coeffG*g+coeffB*b).toInt
+        var misNiveau=(coeffR*r+coeffG*g+coeffB*b).toInt //pour chaque pixel, on fait la moyenne pondérée des trois couleurs rgb
         src(i)(j) = convertBack(t,misNiveau,misNiveau,misNiveau)
       }
     }
   }
   
   def convert(n:Int):(Int,Int,Int,Int)={
-  
+    /*converti l'entier représentant le pixel en un quadruplet (transparence,rouge,vert,bleu)
+     * on fait pour cela du décalage de bits
+     */
     var t = (n >> 24) & 0xFF
     var r = (n >> 16) & 0xFF
     var g = (n >> 8) & 0xFF
@@ -28,6 +33,7 @@ object part1 {
   }
   
   def convertBack(t:Int,r:Int,g:Int,b:Int):Int={
+    //fait l'inverse de convert : part du quadruplet (t,r,g,b) pour donner l'entier correspondant
     return ((t << 24) + (r << 16) + (g << 8) + b)
   }
 }
